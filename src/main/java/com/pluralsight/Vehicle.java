2a5b6d58d061196f43cad658b444ce37ab30ac80
Family <mh5212357@gmail.com>
2025-10-27 14:14:35 -0400

    I finished parent class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,96 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
+
 public class Vehicle {
+
+    private String model;
+    private String color;
+    private int topSpeed;
+    private int fuelCapacity;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+public void startEngine(){
+    System.out.println(" Start the engine is on");
 }
+public void stopEngine(){
+    System.out.println("Stop the engine");
+}
+
+public void accelerate(){
+    System.out.println("Press accelerate");
+}
+public void brake(){
+    System.out.println("Press the brake you can stop car");
+}
+
+}
+
+
+   /* startEngine()
+    stopEngine()
+    accelerate()
+    brake()
+    C) Moped
+    maxWeight: int
+    ride()
+    C) Car
+    numberOfDoors: int
+    openTrunk()
+    closeTrunk()
+(C) Semi Truck
+    numberOfTrailers: int
+    loadCargo()
+    unloadCargo()
+    C) HoverCraft
+    airCushionPressure: int
+⚫ hover()
+}*/
